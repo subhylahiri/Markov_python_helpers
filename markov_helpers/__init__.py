ac8a0b91ec803ef58d3b0051246f2635a8b5d087
Subhaneil Lahiri <subhylahiri@gmail.com>
2021-12-04 01:34:08 -0800

    back to normal

--- markov_helpers/__init__.py
+++ markov_helpers/__init__.py
@@ -1,4 +1,13 @@
 """Utilities for Markov processes.
+
+.. autosummary::
+   :toctree: markov_helpers
+   :recursive:
+
+   markov
+   graphs
+   options
+   utilities
 """
 from . import markov, graphs, options, utilities
 __all__ = [
