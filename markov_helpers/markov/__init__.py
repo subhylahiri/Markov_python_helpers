d30abcc00e561c1ad60f83ba3f95d41f79b0fda1
Subhaneil Lahiri <subhylahiri@gmail.com>
2021-12-03 23:18:21 -0800

    docstring cleanup

--- markov_helpers/markov/__init__.py
+++ markov_helpers/markov/__init__.py
@@ -6,24 +6,24 @@
 """
 from . import indices, params
 from .options import TopologyOptions
-from ._helpers import stochastify_c, stochastify_d, unstochastify_c
-from .markov import (adjoint, calc_peq, calc_peq_d, isstochastic_c,
-                     isstochastic_d, mean_dwell, rand_trans, rand_trans_d,
-                     sim_markov_c, sim_markov_d)
+from ._helpers import (stochastify_c, stochastify_d, stochastify_pd,
+                       unstochastify_c)
+from .markov import (isstochastic_c, isstochastic_d, rand_trans, rand_trans_d,
+                     calc_peq, calc_peq_d, sim_markov_c, sim_markov_d,
+                     adjoint, mean_dwell)
 __all__ = [
    "indices",
    "params",
    "stochastify_c",
    "stochastify_d",
+   'stochastify_pd',
    "unstochastify_c",
    "isstochastic_c",
    "isstochastic_d",
    "rand_trans",
    "rand_trans_d",
-   "adjoint",
    "calc_peq",
    "calc_peq_d",
-   "mean_dwell",
    "sim_markov_c",
    "sim_markov_d",
    "TopologyOptions",
